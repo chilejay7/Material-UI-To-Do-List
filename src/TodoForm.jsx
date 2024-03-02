2211442c51dc020a753946e7e37d5285ab00e17d
chilejay7 <codyburk7@yahoo.com>
2024-03-02 14:58:11 -0700

    adds create button / icon to input field

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,4 +1,5 @@
-import { ListItem, TextField } from "@mui/material";
+import { ListItem, TextField, InputAdornment, IconButton } from "@mui/material";
+import Create from "@mui/icons-material/Create";
 import { useState } from "react";
 
 
@@ -12,7 +13,38 @@ export default function TodoForm() {
 
     return (
         <ListItem>
-            <TextField id="filled-basic" label="Filled" variant="filled" onChange={handleChange} value={text} />
+            <TextField id="filled-basic" label="Filled" variant="filled" onChange={handleChange} value={text}
+                InputProps={{
+                    endAdornment: (
+                        <InputAdornment position="end">
+                            <IconButton
+                                aria-label="toggle password visibility"
+                                edge="end"
+                            >
+                                <Create />
+                            </IconButton>
+                        </InputAdornment>
+                    )
+                }}
+            />
         </ListItem>
     )
 }
+
+// <OutlinedInput
+//             id="outlined-adornment-password"
+//             type={showPassword ? 'text' : 'password'}
+//             endAdornment={
+//               <InputAdornment position="end">
+//                 <IconButton
+//                   aria-label="toggle password visibility"
+//                   onClick={handleClickShowPassword}
+//                   onMouseDown={handleMouseDownPassword}
+//                   edge="end"
+//                 >
+//                   {showPassword ? <VisibilityOff /> : <Visibility />}
+//                 </IconButton>
+//               </InputAdornment>
+//             }
+//             label="Password"
+//           />
